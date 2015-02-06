5616d165eb8e65f7097797728386eeecbd707dcc
Wade Womersley <wade85@gmail.com>
2015-02-06 12:11:59 +0000

    Initial Commit - Interfaces

---
+++ src/Interfaces/LoggerInterface.php
@@ -0,0 +1,16 @@
+<?php
+namespace com\xcitestudios\Logging\Interfaces;
+
+/**
+ * Interface for a provider who can handle LogMessage's.
+ */
+interface LoggerInterface
+{
+	/**
+	 * Handle the provided log message however it needs to be handled.
+	 * 
+	 * @param LogMessageInterface Message to handle
+	 * @return void
+	 */
+	public function log(LogMessageInterface message);
+}
